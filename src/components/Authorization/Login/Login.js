ef7e486966bee782c6bf9c4e7b8e7b160cfe7ea6
Maxim <syncmaxim@gmail.com>
2020-01-23 17:59:20 +0300

    CMT-16. Rebuild login and reg form with formik

--- src/components/Authorization/Login/Login.js
+++ src/components/Authorization/Login/Login.js
@@ -1,61 +1,58 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { Button, TextField } from '@material-ui/core';
 import { useLastLocation } from "react-router-last-location";
-import { openErrorSnackBar, signIn } from "../../../redux/actions";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { signIn } from "../../../redux/actions";
 import '../index.css';
 import { emailValidate } from "../../../utils/helpers";
+import ErrorHandler from "../../shared/ErrorHandler/ErrorHandler";
 
 const Login = props => {
   const dispatch = useDispatch();
   const lastLocation = useLastLocation();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [formErrors, setFormErrors] = useState({
-    email: false,
-    password: false
-  });
 
-  function onLoginSubmit(event) {
-    event.preventDefault();
+  const handleErrors = (values) => {
+    const errors = {};
 
-    if (!email || emailValidate(email)) {
-      setFormErrors(prevState => {
-        dispatch(openErrorSnackBar(`Invalid email address`));
-        return {...prevState, email: true }
-      });
-    } else {
-      if (!password) {
-        setFormErrors(prevState => {
-          dispatch(openErrorSnackBar(`Password field is empty`));
-          return {...prevState, password: true }
-        });
-      }
-    }
+    if (!values.email) errors.email = 'Email address is required';
+    if (emailValidate(values.email)) errors.email = 'Invalid email address';
 
-    if ((email && !formErrors.email) && (password && !formErrors.password)) {
-      dispatch(signIn({email: email, password: password}, lastLocation, props))
-    }
-  }
+    if (!values.password) errors.password = 'Password is required';
+
+    return errors;
+  };
 
   return (
     <div className='authorization-container'>
-      <form>
-        <h2> Log in </h2>
-        <div className='authorization-card'>
-          <div className='form-field'>
-            <TextField error={formErrors.email} name='email' label="Email" onChange={e => {setFormErrors(prevState => ({...prevState, email: false})); setEmail(e.target.value)}} />
-          </div>
-          <div className='form-field'>
-            <TextField error={formErrors.password} name='password' label="Password" type="password" autoComplete="current-password" onChange={e => {setFormErrors(prevState => ({...prevState, password: false})); setPassword(e.target.value)}} />
-          </div>
-          <div className='authorization-controls'>
-            <Button variant="contained" color="primary" onClick={onLoginSubmit}> Log In </Button>
-            <div className='signup-link'> Don't have an account? <Link to='/registration'> Sign up </Link> </div>
-          </div>
-        </div>
-      </form>
+      <h2> Log in </h2>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validate={values => handleErrors(values)}
+        onSubmit={data => {
+          dispatch(signIn(data, lastLocation, props))
+        }}
+      >
+        {({ handleSubmit, errors, touched }) => (
+          <Form onSubmit={handleSubmit}>
+            <div className='authorization-card'>
+              <div className='form-field'>
+                <Field error={touched.email && !!errors.email} type="email" name="email" label="Email" as={TextField} />
+                <ErrorMessage name="email" component={ErrorHandler} />
+              </div>
+              <div className='form-field'>
+                <Field error={touched.password && !!errors.password} type="password" name="password" label="Password" as={TextField} />
+                <ErrorMessage name="password" component={ErrorHandler} />
+              </div>
+              <div className='authorization-controls'>
+                <Button variant="contained" color="primary" type="submit"> Log In </Button>
+                <div className='signup-link'> Don't have an account? <Link to='/registration'> Sign up </Link> </div>
+              </div>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
