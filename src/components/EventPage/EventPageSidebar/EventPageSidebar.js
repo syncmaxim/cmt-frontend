0ee3537582fd515f5002ee80cdea29fde7f669a3
Maxim <syncmaxim@gmail.com>
2020-02-06 15:44:33 +0300

    CMT-24. Rewrite events models for making refs with typegoose

--- src/components/EventPage/EventPageSidebar/EventPageSidebar.js
+++ src/components/EventPage/EventPageSidebar/EventPageSidebar.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Button, Chip } from '@material-ui/core';
 import moment from 'moment';
 
 const AttendButton = (props) => {
-    if (props.attenders === undefined) {
+    if (props.attenders === undefined) { // TODO: PREVENT GETTING UNDEFINED VALUE
         return null;
     }
 
-    if (props.attenders.filter(id => id === props.userId).length === 0) {
+    if (props.attenders.filter(user => user._id === props.userId).length === 0) {
         return <Button variant="outlined" color="primary" onClick={() => props.handleAttend({status: true })}> I want to attend </Button>
     } else {
         return <Button variant="outlined" color="secondary" onClick={() => props.handleAttend({status: false })}> I will be there </Button>
     }
+};
+
+const AttendersList = (props) => {
+    if (props.attenders === undefined) { // TODO: PREVENT GETTING UNDEFINED VALUE
+        return null;
+    }
+
+    const othersLabel = (length) => {
+        return `and other ${ length - 5 }`
+    };
+
+    return (
+        <div className='attenders-list-block'>
+            {
+                props.attenders.map((user, index, array) => {
+                    if (index < 5) {
+                        return <span key={user._id} className='attender-badge'> <Chip color="secondary" size="small" label={user.email} /> </span>
+                    } else if (index === array.length - 1) {
+                        return <span key={index} className='attender-badge'> <Chip size="small" label={othersLabel(array.length)} /> </span>
+                    } else {
+                        return null;
+                    }
+                })
+            }
+        </div>
+    )
 };
 
 const EventPageSidebar = (props) => {
@@ -36,9 +62,7 @@ const EventPageSidebar = (props) => {
             </div>
             <div className='event-sidebar-attendance'>
                 <AttendButton attenders={attenders} userId={props.userData._id} handleAttend={props.handleAttend} />
-                <div>
-                    {/* Here is going to be list of attenders */}
-                </div>
+                <AttendersList attenders={attenders} />
             </div>
         </div>
     );
